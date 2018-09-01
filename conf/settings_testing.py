db863aaeb6b6925fe2159dd1345aad90f363bcc4
unknown <nhmichael@sina.com>
2018-09-01 09:42:34 +0800

    testsetting

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -19,8 +19,8 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME':  'cyctest1test',                        # 数据库名 (默认与APP_ID相同)
-        'USER': ' cyctest1test',                            # 你的数据库user
+        'NAME':  'cyctest1',                        # 数据库名 (默认与APP_ID相同)
+        'USER': ' cyctest1',                            # 你的数据库user
         'PASSWORD': '123456',                        # 你的数据库password
         'HOST': '172.50.21.22',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
