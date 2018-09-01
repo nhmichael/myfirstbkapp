38e6c4f94ebb76c4216f7da25edba10e3f5c4c64
unknown <nhmichael@sina.com>
2018-09-01 10:51:10 +0800

    userpass

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -20,7 +20,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME':  'cyctest1',                        # 数据库名 (默认与APP_ID相同)
-        'USER': ' cyctest1',                            # 你的数据库user
+        'USER': 'cyctest1',                            # 你的数据库user
         'PASSWORD': '123456',                        # 你的数据库password
         'HOST': '172.50.21.22',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
